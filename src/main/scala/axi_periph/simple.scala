e01879f221733a10447038da878b8bc01ffc61d8
meta <eigenform@users.noreply.github.com>
2022-08-07 15:39:08 -0500

    axi_periph: fix addressing, invalid transactions

--- src/main/scala/axi_periph/simple.scala
+++ src/main/scala/axi_periph/simple.scala
@@ -9,7 +9,7 @@ import zno.amba.axi._
 class SimpleDeviceTop extends Module {
 
   // Port for accepting transactions on the Zynq PS7 AXI bus
-  val axi0_port_sink = IO(Flipped(new AXIExternalPort(4, 32)))
+  val axi0_port_sink = IO(Flipped(new AXIExternalPort(32, 32)))
     .suggestName("S_AXI0")
 
   // Connect our logic to the AXI port
@@ -19,10 +19,10 @@ class SimpleDeviceTop extends Module {
 }
 
 
-// This is just a simple 16-word read-only memory addressible on the AXI bus. 
+// This is just a simple read-only memory addressible on the AXI bus. 
 class AXIRegisterDevice extends Module {
   val io = IO(new Bundle {
-    val axi = new AXISinkPort(4, 32)
+    val axi = new AXISinkPort(32, 32)
   })
 
   object TransferState extends ChiselEnum {
@@ -42,13 +42,31 @@ class AXIRegisterDevice extends Module {
 
   // Register for the state machine
   val state     = RegInit(TransferState.IDLE)
-  // Register for the response data
-  val data_reg  = RegInit(0.U(32.W))
+
+  // Register for the read data response channel data. 
+  val resp_reg  = RegInit({
+    val rdata   = Wire(new AXIReadDataChannel(4, 32))
+    rdata.data := 0.U
+    rdata.resp := AXIRespType.OKAY
+    rdata.id   := 0.U
+    rdata.last := false.B
+    rdata
+  })
+
+  // Indicates when a transaction is valid
+  val valid_req = Wire(Bool())
+
+  // Carries an index into the memory
+  val offset    = Wire(UInt(4.W))
 
   // We're always ready to handle a read address in the IDLE state
   // We're always trasmitting valid read response data in the RESP state
   io.axi.raddr.ready := (state === TransferState.IDLE)
   io.axi.rdata.valid := (state === TransferState.RESP)
+
+  // This memory is only addressible in 32-bit words.
+  // For 16 elements, we only need to look at addr[5:2]. 
+  offset := io.axi.raddr.bits.addr(5, 2)
 
   // Always [initially] drive these values on the read data channel.
   // These will be changed when we're actually responding with some data.
@@ -57,30 +75,47 @@ class AXIRegisterDevice extends Module {
   io.axi.rdata.bits.resp    := AXIRespType.OKAY;
   io.axi.rdata.bits.last    := false.B
 
+  // We only handle 4-byte single-transfer transactions.
+  // The PROT/LOCK/CACHE/QOS signals are ignored.
+  valid_req := (io.axi.raddr.bits.size === 4.U) && 
+    (io.axi.raddr.bits.len === 1.U) && 
+    (io.axi.raddr.bits.burst === AXIBurstType.FIXED)
+
+  switch (state) {
+
+    // Latch the appropriate response for this transaction.
+    // The response is driven on the next cycle. 
+    is (TransferState.IDLE) {
+      when (io.axi.raddr.fire) {
+        state     := TransferState.RESP
+        when (valid_req) {
+          resp_reg.data := mem(offset)
+          resp_reg.resp := AXIRespType.OKAY
+          resp_reg.id   := io.axi.raddr.bits.id
+          resp_reg.last := true.B
+        }.otherwise {
+          resp_reg.data := 0.U
+          resp_reg.resp := AXIRespType.SLVERR
+          resp_reg.id   := io.axi.raddr.bits.id
+          resp_reg.last := true.B
+        }
+      }
+    }
+
+    // Reply with the registered response.
+    is (TransferState.RESP) {
+      when (io.axi.rdata.fire) {
+        state := TransferState.IDLE
+        io.axi.rdata.bits := resp_reg
+      }
+    }
+  }
+
   // Leave the write channels unconnected for now (i.e. do nothing)
   io.axi.waddr              := DontCare
   io.axi.wdata              := DontCare
   io.axi.wresp              := DontCare
 
-  switch (state) {
-    // Use the address to latch some data into the response register.
-    is (TransferState.IDLE) {
-      when (io.axi.raddr.fire) {
-        state     := TransferState.RESP
-        data_reg  := mem(io.axi.raddr.bits.addr)
-      }
-    }
-    // Read the response register and reply with the data.
-    is (TransferState.RESP) {
-      when (io.axi.rdata.fire) {
-        state := TransferState.IDLE
-        io.axi.rdata.bits.data := data_reg
-        io.axi.rdata.bits.resp := AXIRespType.OKAY
-        io.axi.rdata.bits.id   := io.axi.raddr.bits.id
-        io.axi.rdata.bits.last := true.B
-      }
-    }
-  }
 }
 
 
